cae4f6ad1a8aa17041007966df21e77cc747708b
PotatoPresident <potatoboy9999@outlook.com>
2021-01-04 16:37:23 -0800

    Buffed Heal Module Gave Heal Module particles

--- src/main/java/us/potatoboy/fortress/custom/item/HealModuleItem.java
+++ src/main/java/us/potatoboy/fortress/custom/item/HealModuleItem.java
@@ -5,15 +5,21 @@ import it.unimi.dsi.fastutil.objects.Object2ObjectMaps;
 import net.minecraft.entity.effect.StatusEffectInstance;
 import net.minecraft.entity.effect.StatusEffects;
 import net.minecraft.item.Item;
+import net.minecraft.particle.DustParticleEffect;
 import net.minecraft.server.network.ServerPlayerEntity;
 import net.minecraft.server.world.ServerWorld;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.MathHelper;
+import net.minecraft.util.math.Vec3d;
 import net.minecraft.world.GameMode;
+import us.potatoboy.fortress.game.FortressSpawnLogic;
 import us.potatoboy.fortress.game.active.FortressPlayer;
 import xyz.nucleoid.plasmid.game.player.GameTeam;
 import xyz.nucleoid.plasmid.util.BlockBounds;
 import xyz.nucleoid.plasmid.util.PlayerRef;
+
+import java.util.Random;
 
 public class HealModuleItem extends ModuleItem{
     public HealModuleItem(Item proxy, Identifier structure) {
@@ -22,7 +28,7 @@ public class HealModuleItem extends ModuleItem{
 
     @Override
     public void tick(BlockPos center, Object2ObjectMap<PlayerRef, FortressPlayer> participants, GameTeam owner, ServerWorld world) {
-        BlockBounds bounds = new BlockBounds(center.add(-1, 0, -1), center.add(1, 0, 1));
+        BlockBounds bounds = new BlockBounds(center.add(-4, 0, -4), center.add(4, 4, 4));
 
         for (Object2ObjectMap.Entry<PlayerRef, FortressPlayer> entry : Object2ObjectMaps.fastIterable(participants)) {
             ServerPlayerEntity player = entry.getKey().getEntity(world);
@@ -34,5 +40,33 @@ public class HealModuleItem extends ModuleItem{
             StatusEffectInstance effectInstance = new StatusEffectInstance(StatusEffects.REGENERATION, 30, 1, true, true, true);
             player.addStatusEffect(effectInstance);
         }
+
+        Random random = new Random();
+        DustParticleEffect effect = new DustParticleEffect(0.99f, 0.49f, 0.61f, 2);
+        for (int i = 0; i < 10; i++) {
+
+            Vec3d pos = randomPos(random, bounds);
+
+            world.spawnParticles(
+                    effect,
+                    pos.getX() + 0.5,
+                    pos.getY() + 1,
+                    pos.getZ() + 0.5,
+                    1,
+                    0.0, 0.0, 0.0,
+                    0.0
+            );
+        }
+    }
+
+    public static Vec3d randomPos(Random random, BlockBounds bounds) {
+        BlockPos min = bounds.getMin();
+        BlockPos max = bounds.getMax();
+
+        double x = MathHelper.nextDouble(random, min.getX(), max.getX());
+        double z = MathHelper.nextDouble(random, min.getZ(), max.getZ());
+        double y = MathHelper.nextDouble(random, min.getY(), max.getY());
+
+        return new Vec3d(x, y, z);
     }
 }
